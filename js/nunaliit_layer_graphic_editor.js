8628b78b3fdbb646694a4e5bee1c41dd11613f27
Robert Oikle <6431161+roikle@users.noreply.github.com>
2015-11-09 15:42:27 -0500

    Click summary is reported to the textarea box

--- js/nunaliit_layer_graphic_editor.js
+++ js/nunaliit_layer_graphic_editor.js
@@ -25,10 +25,10 @@ graphicEditor.generateGrid = function() {
     };
 };
 
-//Function Name: generateAxisLabels
-//Description: Creates x and y axis grid labels Note: axis origin is top right corner
+// Function Name: generateAxisLabels
+// Description: Creates x and y axis grid labels Note: axis origin is top right corner
 graphicEditor.generateAxisLabels = function() {
-    //Generate x and y axis labels
+    // Generate x and y axis labels
     $('#grid').append('<div class="col" id="yaxis"></div');
     for (var i = 0; i <= 9; i++) {
         $('#yaxis').append('<div class="labelcell">' + i + '</div>');
@@ -52,15 +52,18 @@ graphicEditor.initiateGridModel = function() {
 
 // Function Name: resetGridModel
 // Description: Reset the grid model to the initial values. 
-graphicEditor.resetGridmodel = function() {  
-    
-    //Iterate through all grid cells and set click state to false
+graphicEditor.resetGridmodel = function() {
+    // Iterate through all grid cells and set click state to false
     for (var cell in this.gridModel) {
         this.gridModel[cell].clickState = false;
-    };    
-    //Update Grid view
+    };
+    // Reset click summary 
+    this.userClickSummary = "";
+    graphicEditor.updateTextarea();
+    
+    // Update Grid view
     graphicEditor.updateGridView();    
-}
+};
 
 // Function Name: updateGridModel
 // Description: updates cell object properties
@@ -70,8 +73,8 @@ graphicEditor.updateGridModel = function(update) {
     var updateGrid = false;
     
     for (var cell in this.gridModel) {
-        //if cell was not previously clicked, the clickState is set to true
-        //else if cell was previously clicked, the clickState reverts to false.
+        // if cell was not previously clicked, the clickState is set to true
+        // else if cell was previously clicked, the clickState reverts to false.
         if (cellID === this.gridModel[cell].cellID && this.gridModel[cell].clickState === false) {
             this.gridModel[cell].clickState = true;
             updateGrid = true;
@@ -79,15 +82,14 @@ graphicEditor.updateGridModel = function(update) {
             this.gridModel[cell].clickState = false;
             updateGrid = true;
         };     
-    };
-    
+    };    
     if (updateGrid) {
         graphicEditor.updateGridView();
-    }
+    };
 };
 
-//Function Name: updateGridView
-//Description: updates the class of each div cell based on the state in the grid data model
+// Function Name: updateGridView
+// Description: updates the class of each div cell based on the state in the grid data model
 graphicEditor.updateGridView = function() {
     for (var cell in this.gridModel) {
         if (this.gridModel[cell].clickState === true)    {      
@@ -98,17 +100,14 @@ graphicEditor.updateGridView = function() {
     };
 };
 
-
-//Function Name: gridEventListener
-//Description: Initiate each of the event listener functions
+// Function Name: gridEventListener
+// Description: Initiate each of the event listener functions
 graphicEditor.gridEventListener = function() {
-    //Handler for clicking on grid cells
+    // Handler for clicking on grid cells
     this.selectCell();
-    
-    //Handler for hovering over grid cells
+    // Handler for hovering over grid cells
     this.mouseoverCell();  
-    
-    //Handler for hovering off grid cells
+    // Handler for hovering off grid cells
     this.unselectCell();
 };
 
@@ -117,16 +116,20 @@ graphicEditor.gridEventListener = function() {
 graphicEditor.selectCell = function() {
     $('.gridcell').click( function() { 
         var cellID = $(this).attr('id');
-        //window.console.log("Cell ID: " + cellID + " mouse click event");
+        // window.console.log("Cell ID: " + cellID + " mouse click event");
         
-        //define update
+        // define update
         var update = {
           cellID:cellID,       
           clickState:true
         };
-        
-        //execute grid model update
-        graphicEditor.updateGridModel(update);        
+        // execute grid model update
+        graphicEditor.updateGridModel(update);
+        // update clickSummary
+        var coordPairs = cellID.split("_");
+        graphicEditor.updateClickSummary(coordPairs);
+        // update textArea
+        graphicEditor.updateTextarea();
     });
 };
 
@@ -148,19 +151,37 @@ graphicEditor.mouseoverCell = function() {
     });    
 };
 
+// Function Name: updateClickSummary
+// Description: update the click summary string
+graphicEditor.updateClickSummary = function(coords) {
+    var newCoordsPair = coords[1] + "," + coords[0];
+    if (this.userClickSummary === "") {
+        this.userClickSummary = this.userClickSummary + newCoordsPair;        
+    } else {
+        this.userClickSummary = this.userClickSummary + "," + newCoordsPair;
+    };
+};
+
+// Function Name: updateTextarea
+// Description: updates value of the input form textarea element
+graphicEditor.updateTextarea = function() {
+    $('#clickSummary').val("[" + this.userClickSummary + "]");
+};
 
 // Function name: start
 // Description: calls required functions to start graphic editor. 
 graphicEditor.start = function() {
-    //Create div grid  
+    // Create div grid  
     this.generateGrid();
-    //Add grid axis labels
+    // Add grid axis labels
     this.generateAxisLabels();
-    //Initiate Grid data model
+    // Initiate erid data model
     this.initiateGridModel();
-    //Grid Event listener
+    // Grid event listener
     this.gridEventListener();
+    // Set text area
+    this.updateTextarea();
 };
 
-//Start editor
+// Start editor
 graphicEditor.start();
