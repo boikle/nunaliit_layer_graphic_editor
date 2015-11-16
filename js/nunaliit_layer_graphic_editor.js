bf8a5ff8eb849af2ecf2fc6fdf855add320d5e9c
Robert Oikle <6431161+roikle@users.noreply.github.com>
2015-11-16 14:30:26 -0500

    Updated instructions, controls and point connecting function.

--- js/nunaliit_layer_graphic_editor.js
+++ js/nunaliit_layer_graphic_editor.js
@@ -1,13 +1,21 @@
 var graphicEditor = graphicEditor || {
     gridModel: [],
-    numColumns: 10,
-    numRows: 10,
+    numColumns: 11,
+    numRows: 11,
     userClickSummary: "", 
     map: null,
     mercator: null,
     WGS84: null,
-    centerPosition: null
+    centerPosition: null,
+    defaultStyle: null,
+    appStyleMap: null,
+    pointLayer: null,
+    point: null,
+    pointFeature: null
 };
+
+
+// Constructors //////////////////////////////////////////////////////////////////////////
 
 // Function Name: cell
 // Description: constructor for creating cell objects
@@ -15,16 +23,19 @@ function cell(id) {
     this.clickState = false;
     this.cellID = id;
 };
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+// Grid editor functions
 
 // Function Name: generateGrid
-// Description: Produces a 10x10 grid consisting of 20x20 div cells
+// Description: Produces a 11x11 grid consisting of 20x20 px div cells
 graphicEditor.generateGrid = function() {
     var numColumns = this.numColumns;
     var numRows = this.numRows;
-    for (var i = numColumns; i > 0; i--) {
-        $('#grid').append('<div class="col" id="col_'+ (i-1) +'"></div');
-        for (var j = 0; j < numRows; j++) {
-            $('#col_' + (i-1)).append('<div class="gridcell" id="' + j + "_" + (i-1) +'"></div>');
+    for (var i = 0; i < numRows; i++) {
+        $('#grid').append('<div class="col" id="col_'+ i +'"></div');
+        for (var j = 0; j < numColumns; j++) {
+            $('#col_' + i).append('<div class="gridcell" id="' + i + "_" + j +'"></div>');
         };
     };
 };
@@ -34,11 +45,27 @@ graphicEditor.generateGrid = function() {
 graphicEditor.generateAxisLabels = function() {
     // Generate x and y axis labels
     $('#grid').append('<div class="col" id="yaxis"></div');
-    for (var i = 0; i <= 9; i++) {
+    for (var i = 0; i <= (this.numRows - 1); i++) {
         $('#yaxis').append('<div class="labelcell">' + i + '</div>');
         $('#col_' + i).append('<div class="labelcell">' + i + '</div>');
     };
 };
+
+//Function Name: updateGridView
+//Description: updates the class of each div cell based on the state in the grid data model
+graphicEditor.updateGridView = function() {
+    for (var cell in this.gridModel) {
+         if (this.gridModel[cell].clickState === true)    {      
+                 $('#'+this.gridModel[cell].cellID).addClass('selectedgridcell');
+         } else if (this.gridModel[cell].clickState === false) {
+                 $('#'+this.gridModel[cell].cellID).removeClass('selectedgridcell');
+         };
+     };
+};
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+
+// Grid Model functions
 
 // Function Name: initiateGridModel
 // Description: Initiate grid model with unselected cell values
@@ -47,8 +74,8 @@ graphicEditor.initiateGridModel = function() {
     var numColumns = this.numColumns;
     var numRows = this.numRows;  
     for (var i = 0; i < numRows; i ++) {
-        for(var j = 0; j < numColumns; j ++) {
-            var cellModel = new cell(j + "_" + i);
+        for(var j = 0; j < this.numColumns; j ++) {
+            var cellModel = new cell(i + "_" + j);
             gridModel.push(cellModel);  
         };      
     };
@@ -56,17 +83,11 @@ graphicEditor.initiateGridModel = function() {
 
 // Function Name: resetGridModel
 // Description: Reset the grid model to the initial values. 
-graphicEditor.resetGridmodel = function() {
+graphicEditor.resetGridModel = function() {
     // Iterate through all grid cells and set click state to false
     for (var cell in this.gridModel) {
         this.gridModel[cell].clickState = false;
     };
-    // Reset click summary 
-    this.userClickSummary = "";
-    graphicEditor.updateTextarea();
-    
-    // Update Grid view
-    graphicEditor.updateGridView();    
 };
 
 // Function Name: updateGridModel
@@ -82,37 +103,28 @@ graphicEditor.updateGridModel = function(update) {
         if (cellID === this.gridModel[cell].cellID && this.gridModel[cell].clickState === false) {
             this.gridModel[cell].clickState = true;
             updateGrid = true;
-        } else if (cellID === this.gridModel[cell].cellID && this.gridModel[cell].clickState === true){
-            this.gridModel[cell].clickState = false;
-            updateGrid = true;
-        };     
+        }; 
     };    
     if (updateGrid) {
         graphicEditor.updateGridView();
     };
 };
+// ///////////////////////////////////////////////////////////////////////////////////////
 
-// Function Name: updateGridView
-// Description: updates the class of each div cell based on the state in the grid data model
-graphicEditor.updateGridView = function() {
-    for (var cell in this.gridModel) {
-        if (this.gridModel[cell].clickState === true)    {      
-                    $('#'+this.gridModel[cell].cellID).addClass('selectedgridcell');
-        } else if (this.gridModel[cell].clickState === false) {
-                    $('#'+this.gridModel[cell].cellID).removeClass('selectedgridcell');
-        };
-    };
-};
+
+// Event handling functions
 
 // Function Name: gridEventListener
 // Description: Initiate each of the event listener functions
 graphicEditor.gridEventListener = function() {
-    // Handler for clicking on grid cells
     this.selectCell();
-    // Handler for hovering over grid cells
     this.mouseoverCell();  
-    // Handler for hovering off grid cells
-    this.unselectCell();
+    this.unselectCell();    
+    this.fillColourChange();
+    this.fillOpacityChange();
+    this.strokeColourChange();
+    this.strokeOpacityChange();
+    this.symbolSizeChange();
 };
 
 // Function Name: selectCell
@@ -120,7 +132,6 @@ graphicEditor.gridEventListener = function() {
 graphicEditor.selectCell = function() {
     $('.gridcell').click( function() { 
         var cellID = $(this).attr('id');
-        // window.console.log("Cell ID: " + cellID + " mouse click event");
         
         // define update
         var update = {
@@ -134,6 +145,8 @@ graphicEditor.selectCell = function() {
         graphicEditor.updateClickSummary(coordPairs);
         // update textArea
         graphicEditor.updateTextarea();
+        // set required cells as clicked to connect points
+        graphicEditor.connectPoints();
     });
 };
 
@@ -152,13 +165,195 @@ graphicEditor.mouseoverCell = function() {
     $('.gridcell').mouseover( function() {    
         var cellID = $(this).attr('id');      
         $(this).toggleClass('mouseovergridcell');        
-    });    
+    });
 };
+
+// Function Name: fillColourChange
+// Description: Update the fill colour of the map style
+graphicEditor.fillColourChange = function() {
+    $('#fillColourSelector').change(function() {
+        graphicEditor.defaultStyle.defaultStyle.fillColor = $('#fillColourSelector').val();
+        graphicEditor.previewGraphic();
+    });
+};
+
+//Function Name: fillOpacityChange
+//Description: Update the fill opacity
+graphicEditor.fillOpacityChange = function() {
+ $('#fillOpacitySelector').change(function() {
+     graphicEditor.defaultStyle.defaultStyle.fillOpacity = $('#fillOpacitySelector').val();
+     graphicEditor.previewGraphic();
+ });
+};
+
+// Function Name: strokeColourChange
+// Description: Update the stroke colour of the map style
+graphicEditor.strokeColourChange = function() {
+    $('#strokeColourSelector').change(function() {
+        graphicEditor.defaultStyle.defaultStyle.strokeColor = $('#strokeColourSelector').val();
+        graphicEditor.previewGraphic();
+    });
+};
+
+//Function Name: strokeOpacityChange
+//Description: Update the stroke opacity
+graphicEditor.strokeOpacityChange = function() {
+ $('#strokeOpacitySelector').change(function() {
+     graphicEditor.defaultStyle.defaultStyle.strokeOpacity = $('#strokeOpacitySelector').val();
+     graphicEditor.previewGraphic();
+ });
+};
+
+// Function Name: symbolSizeChange
+// Description: Update the symbol size
+graphicEditor.symbolSizeChange = function() {
+    $('#pointRadiusSelector').change(function() {
+        graphicEditor.defaultStyle.defaultStyle.pointRadius = $('#pointRadiusSelector').val();
+        graphicEditor.previewGraphic();
+    });
+};
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Function Name: calcSlope
+// Description: calculates slope between two sets of points
+graphicEditor.calcSlope = function(x1,y1,x2,y2) {
+    var rise = y2-y1;
+    var run = x2-x1;
+    var slope = rise/run;
+    return slope;
+};
+
+//Function Name: calcIntercept
+//Description: calculates y intercept
+graphicEditor.calcIntercept = function(x,y,m) {
+    var b = y - (m*x);
+    return b;
+};
+
+//Function Name: calcY
+//Description: calculates y value
+graphicEditor.calcY = function(x,m,b) {
+    var y = Math.round((m*x)+b);
+    return y;
+};
+
+//Function Name: calcY
+//Description: calculates x value
+graphicEditor.calcX = function(y,m,b) {
+    var x = Math.round((y-b)/m);  
+    return x;
+};
+
+// Function Name: connectPoints
+// Description: determines which grid cells between two points need to be set to clickState to true
+graphicEditor.connectPoints = function() {
+    eval("var clickSummary =" + $('#clickSummary').val());
+    
+    if (clickSummary.length >= 4) {        
+        //determine coordinates of last two points
+        var x1 = clickSummary[clickSummary.length - 4];
+        var y1 = clickSummary[clickSummary.length - 3];
+        var x2 = clickSummary[clickSummary.length - 2];
+        var y2 = clickSummary[clickSummary.length - 1];
+        var m = this.calcSlope(x1,y1,x2,y2);
+        var b = this.calcIntercept(x1, y1, m);
+        
+        // update grid model to connect between points
+        if (x1 === x2) {
+            if (y1 < y2) {
+                for (var i = y1; i < y2; i ++)
+                {
+                    var currentCell = x1 + "_" + i;
+                    var update = {
+                            cellID:currentCell,       
+                            clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);
+                }; 
+            } else {
+                for (var i = y2; i < y1; i ++)
+                {
+                    var currentCell = x1 + "_" + i;
+                    var update = {
+                            cellID:currentCell,       
+                            clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);
+                }; 
+            };
+        } else if (y1 === y2) {
+            if (x1 < x2) {
+                for (var i = x1; i < x2; i ++)
+                {
+                    var currentCell = i + "_" + y1;
+                    var update = {
+                            cellID:currentCell,       
+                            clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);
+                }; 
+            } else {
+                for (var i = x2; i < x1; i ++)
+                {
+                    var currentCell = i + "_" + y1;
+                    var update = {
+                            cellID:currentCell,       
+                            clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);
+                }; 
+            };
+            
+        } else if (x1 != x2 || y1 != y2) {
+            if (Math.abs(x2 - x1) > Math.abs(y2 - y1)) {
+                if (x1 > x2) {
+                    var highX = x1;
+                    var lowX = x2;
+                } else {
+                    var highX = x2;
+                    var lowX = x1;
+                };                
+                for (var i = lowX + 1; i != highX; i++) {
+                    var y = this.calcY(i,m,b);
+                    var currentCell = i + "_" + y;
+                    var update = {
+                          cellID:currentCell,       
+                          clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);                 
+                };
+            } else {
+                if (y1 > y2) {
+                    var highY = y1;
+                    var lowY = y2;
+                } else {
+                    var highY = y2;
+                    var lowY = y1;
+                };  
+                for (var i = lowY + 1; i != highY; i++) {
+                    var x = this.calcX(i,m,b);
+                    var currentCell = x + "_" + i;
+                    var update = {
+                          cellID:currentCell,       
+                          clickState:true 
+                    };
+                    graphicEditor.updateGridModel(update);              
+                };                
+            };
+        };
+    };    
+};
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+
+// Control interface functions
 
 // Function Name: updateClickSummary
 // Description: update the click summary string
 graphicEditor.updateClickSummary = function(coords) {
-    var newCoordsPair = coords[1] + "," + coords[0];
+    var newCoordsPair = coords[0] + "," + coords[1];
     if (this.userClickSummary === "") {
         this.userClickSummary = this.userClickSummary + newCoordsPair;        
     } else {
@@ -172,34 +367,121 @@ graphicEditor.updateTextarea = function() {
     $('#clickSummary').val("[" + this.userClickSummary + "]");
 };
 
+// Function Name: reset
+// Description: calls functions to reset the model and view for the user
+graphicEditor.reset = function() {
+    // Reset grid model
+    graphicEditor.resetGridModel();
+    
+    // Reset click summary 
+    this.userClickSummary = "";
+    graphicEditor.updateTextarea();
+    
+    // Update Grid view
+    graphicEditor.updateGridView(); 
+    
+    // Remove point layers from map
+    graphicEditor.removePreviewGraphic();
+    
+    // Reset fill colour
+    this.defaultStyle.defaultStyle.fillColor = '#0000FF';
+    $('#fillColourSelector').val('#0000FF');
+    
+    // Reset fill opacity
+    this.defaultStyle.defaultStyle.fillOpacity = 0.3;
+    $('#fillOpacitySelector').val(0.3);
+    
+    // Reset stroke colour
+    this.defaultStyle.defaultStyle.strokeColor = '#000F76';
+    $('#strokeColourSelector').val('#000F76');
+    
+    // Reset stroke opacity
+    this.defaultStyle.defaultStyle.strokeOpacity = 1;
+    $('#strokeOpacitySelector').val(1);
+    
+    // Reset point radius
+    this.defaultStyle.defaultStyle.pointRadius = 12;
+    $('#pointRadiusSelector').val(12);
+};
+// ///////////////////////////////////////////////////////////////////////////////////////
+
+// Preview map functions
+
+//Function Name: initDefaultStyle
+//Description: initiate the default style of a symbol
+graphicEditor.initDefaultStyle = function() {
+ this.defaultStyle = new OpenLayers.Style({
+     graphicName: "previewSymbol",
+     fillColor: '#0000FF',
+     strokeColor: '#000F76',
+     strokeOpacity: 1.0,
+     fillOpacity: 0.3,
+     pointRadius: 12
+ });    
+};
+
 // Function Name: initPreviewMap
 // Description: Initiates the preview map
 graphicEditor.initPreviewMap = function() {
     this.map = new OpenLayers.Map('map');
     this.mercator = new OpenLayers.Projection("EPSG:900913"); 
     this.WGS84 = new OpenLayers.Projection("EPSG:4326");
-    this.centerPosition = new OpenLayers.LonLat(-75.3, 45.6);
+    this.centerPosition = new OpenLayers.LonLat(-75.69953, 45.38076);
     
     // define background layer
     var osm = new OpenLayers.Layer.OSM( "Open Street Map");
     this.map.addLayers([osm]); 
     
     // define center position and zoom level
-    this.map.setCenter(this.centerPosition.transform(this.WGS84, this.mercator), 7);
+    this.map.setCenter(this.centerPosition.transform(this.WGS84, this.mercator), 10);
+    
+    //initiate default graphic style
+    this.initDefaultStyle
+    
+    this.appStyleMap = new OpenLayers.StyleMap({
+        default: this.defaultStyle
+    });
+};
+
+// Function Name: updatePreviewSymbol
+// Description: update the preview symbol 
+graphicEditor.updatePreviewSymbol = function() {
+    eval('OpenLayers.Renderer.symbol.previewSymbol =' + $('#clickSummary').val());
 };
 
 // Function Name: previewGraphic
 // Description: show preview graphic point
 graphicEditor.previewGraphic = function() {
-    var pointLayer = new OpenLayers.Layer.Vector("Point layer");
-    this.map.addLayer(pointLayer); 
     
-    // create and add point to tempPointLayer
-    var point = new OpenLayers.Geometry.Point(-75.6, 45.3);
-    point.transform(this.WGS84, this.mercator);
-    var pointFeature = new OpenLayers.Feature.Vector(point, null);                    
-    pointLayer.addFeatures([pointFeature]);  
+    if ($('#clickSummary').val() != '[]') {
+        //remove any graphic symbols on map prior to adding a new one
+        this.removePreviewGraphic();
+        
+        this.pointLayer = new OpenLayers.Layer.Vector('Point layer', {
+            styleMap: this.appStyleMap        
+        });
+        this.map.addLayer(this.pointLayer);
+        
+        //update Graphic symbol
+        this.updatePreviewSymbol();
+        
+        // create and add point to tempPointLayer
+        this.point = new OpenLayers.Geometry.Point(-75.69953, 45.38076);
+        this.point.transform(this.WGS84, this.mercator);
+        this.pointFeature = new OpenLayers.Feature.Vector(this.point, null);                    
+        this.pointLayer.addFeatures([this.pointFeature]); 
+    };
 };
+
+// Function Name: removePreviewGraphic
+// Description: removes the preview graphic symbol from the map
+graphicEditor.removePreviewGraphic = function() {
+    if (this.pointFeature != null) {
+        this.pointLayer.destroyFeatures([this.pointFeature]);
+    };
+}
+// ///////////////////////////////////////////////////////////////////////////////////////
+
 
 // Function name: start
 // Description: calls required functions to start graphic editor. 
@@ -212,7 +494,9 @@ graphicEditor.start = function() {
     this.initiateGridModel();
     // Grid event listener
     this.gridEventListener();
-    // Initiate OpenLayers 3 preview map
+    // Initiate default style of point
+    this.initDefaultStyle();
+    // Initiate OpenLayers 2 preview map
     this.initPreviewMap();
     // Set text area
     this.updateTextarea();
